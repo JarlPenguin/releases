c722353495d1b2618ee0a648ff495c192607c4e0
Jarl-Penguin <jarlpenguin@outlook.com>
2019-08-07 15:32:35 +0300

    river build support

--- config.sh
+++ config.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="JarlPenguin"
+export GITHUB_EMAIL="jarlpenguin@gmail.com"
 
-export device=""
+export device="river"
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
+export ROM="Pixel Experience"
+export ROM_DIR="${WORKSPACE}/../PixelExperience-eleven/work/rom"
+export ROM_VERSION="11"
 export official="false"
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
+export local_manifest_url="https://raw.githubusercontent.com/JarlPenguin/manifests/master/pe/11/river.xml"
+export manifest_url="https://github.com/PixelExperience/manifest"
+export rom_vendor_name="aosp"
+export branch="eleven"
 export bacon="bacon"
 export buildtype=""
-export clean=""
-export generate_incremental=""
-export upload_recovery=""
+export clean="installclean"
+export generate_incremental="false"
+export upload_recovery="false"
 
-export ccache=""
-export ccache_size=""
+export ccache="true"
+export ccache_size="100"
 
-export jenkins="false"
+export jenkins="true"
 
-export release_repo=""
+export release_repo="JarlPenguin/releases"
 
 export timezone="UTC"

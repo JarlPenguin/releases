ff2a8e37e9e8d431c1d12587673a24379463ee36
JarlPenguin <jarlpenguin@outlook.com>
2019-08-11 18:05:03 +0300

    releases: Make it compatible with TWRP
    Signed-off-by: JarlPenguin <jarlpenguin@outlook.com>

--- build.sh
+++ build.sh
@@ -10,9 +10,6 @@ else
 fi
 source build/envsetup.sh
 source "${my_dir}/config.sh"
-if [ -z "${buildtype}" ]; then
-    export buildtype="userdebug"
-fi
 if [ "${ccache}" == "true" ] && [ -n "${ccache_size}" ]; then
     export USE_CCACHE=1
     ccache -M "${ccache_size}G"
@@ -20,110 +17,33 @@ elif [ "${ccache}" == "true" ] && [ -z "${ccache_size}" ]; then
     echo "Please set the ccache_size variable in your config."
     exit 1
 fi
-if [ ! -z "${rom_vendor_name}" ]; then
-    lunch "${rom_vendor_name}_${device}-${buildtype}"
-else
-    lunch "${device}-${buildtype}"
-fi
-if [ "${clean}" == "clean" ]; then
-    m clean -j$(nproc --all)
-elif [ "${clean}" == "installclean" ]; then
-    m installclean -j$(nproc --all)
-    rm -rf out/target/product/"${device}"/obj/DTBO_OBJ
-else
-    rm "${outdir}"/*$(date +%Y)*.zip*
-fi
-m "${bacon}" -j$(nproc --all)
+export ALLOW_MISSING_DEPENDENCIES=true
+export LC_ALL=C
+lunch "${rom_vendor_name}_${device}-eng"
+m clean -j$(nproc --all)
+m recoveryimage -j$(nproc --all)
 buildsuccessful="${?}"
 BUILD_END=$(date +"%s")
 BUILD_DIFF=$((BUILD_END - BUILD_START))
 
-if [ "${generate_incremental}" == "true" ]; then
-    if [ -e "${ROM_DIR}"/*"${device}"*target_files*.zip ]; then
-        export old_target_files_exists=true
-        export old_target_files_path=$(ls "${ROM_DIR}"/*"${device}"*target_files*.zip | tail -n -1)
-    else
-        echo "Old target-files package not found, generating incremental package on next build"
-    fi
-    export new_target_files_path=$(ls "${outdir}"/obj/PACKAGING/target_files_intermediates/*target_files*.zip | tail -n -1)
-    if [ "${old_target_files_exists}" == "true" ]; then
-        ota_from_target_files -i "${old_target_files_path}" "${new_target_files_path}" "${outdir}"/incremental_ota_update.zip
-        export incremental_zip_path=$(ls "${outdir}"/incremental_ota_update.zip | tail -n -1)
-    fi
-    cp "${new_target_files_path}" "${ROM_DIR}"
-fi
-if [ -e "${outdir}"/*$(date +%Y)*.zip ]; then
-    export finalzip_path=$(ls "${outdir}"/*$(date +%Y)*.zip | tail -n -1)
-else
-    export finalzip_path=$(ls "${outdir}"/*"${device}"-ota-*.zip | tail -n -1)
-fi
-if [ "${upload_recovery}" == "true" ]; then
-    if [ ! -e "${outdir}"/recovery.img ]; then
-        cp "${outdir}"/boot.img "${outdir}"/recovery.img
-    fi
-    export img_path=$(ls "${outdir}"/recovery.img | tail -n -1)
-fi
-export zip_name=$(echo "${finalzip_path}" | sed "s|${outdir}/||")
-export tag=$( echo "$(date +%Y%m%d%H%M)-${zip_name}" | sed 's|.zip||')
-if [ "${buildsuccessful}" == "0" ] && [ ! -z "${finalzip_path}" ]; then
+export img_path=$(ls "${outdir}"/recovery.img | tail -n -1)
+export tag="twrp-${device}-$(date +%m%d%Y-%H%M)"
+if [ "${buildsuccessful}" == "0" ] && [ -e "${img_path}" ]; then
     echo "Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds"
+
+    cp "${img_path}" "${outdir}/${tag}.img"
 
     echo "Uploading"
 
     github-release "${release_repo}" "${tag}" "master" "${ROM} for ${device}
 
-Date: $(env TZ="${timezone}" date)" "${finalzip_path}"
-    if [ "${generate_incremental}" == "true" ]; then
-        if [ -e "${incremental_zip_path}" ] && [ "${old_target_files_exists}" == "true" ]; then
-            github-release "${release_repo}" "${tag}" "master" "${ROM} for ${device}
+Date: $(env TZ="${timezone}" date)" "${outdir}/${tag}.img"
 
-Date: $(env TZ="${timezone}" date)" "${incremental_zip_path}"
-        elif [ ! -e "${incremental_zip_path}" ] && [ "${old_target_files_exists}" == "true" ]; then
-            echo "Build failed in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds"
-            telegram -N -M "Build failed in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds"
-            curl --data parse_mode=HTML --data chat_id=$TELEGRAM_CHAT --data sticker=CAADBQADGgEAAixuhBPbSa3YLUZ8DBYE --request POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendSticker
-            exit 1
-        fi
-    fi
-    if [ "${upload_recovery}" == "true" ]; then
-        if [ -e "${img_path}" ]; then
-            github-release "${release_repo}" "${tag}" "master" "${ROM} for ${device}
-
-Date: $(env TZ="${timezone}" date)" "${img_path}"
-        else
-            echo "Build failed in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds"
-            telegram -N -M "Build failed in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds"
-            curl --data parse_mode=HTML --data chat_id=$TELEGRAM_CHAT --data sticker=CAADBQADGgEAAixuhBPbSa3YLUZ8DBYE --request POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendSticker
-            exit 1
-        fi
-    fi
     echo "Uploaded"
 
-    if [ "${upload_recovery}" == "true" ]; then
-        if [ "${old_target_files_exists}" == "true" ]; then
-            telegram -M "Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds
+    telegram -M "Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds
 
-Download ROM: ["${zip_name}"]("https://github.com/${release_repo}/releases/download/${tag}/${zip_name}")
-Download incremental update: ["incremental_ota_update.zip"]("https://github.com/${release_repo}/releases/download/${tag}/incremental_ota_update.zip")
-Download recovery: ["recovery.img"]("https://github.com/${release_repo}/releases/download/${tag}/recovery.img")"
-        else
-            telegram -M "Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds
-
-Download ROM: ["${zip_name}"]("https://github.com/${release_repo}/releases/download/${tag}/${zip_name}")
-Download recovery: ["recovery.img"]("https://github.com/${release_repo}/releases/download/${tag}/recovery.img")"
-        fi
-    else
-        if [ "${old_target_files_exists}" == "true" ]; then
-            telegram -M "Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds
-
-Download: ["${zip_name}"]("https://github.com/${release_repo}/releases/download/${tag}/${zip_name}")
-Download incremental update: ["incremental_ota_update.zip"]("https://github.com/${release_repo}/releases/download/${tag}/incremental_ota_update.zip")"
-        else
-            telegram -M "Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds
-
-Download: ["${zip_name}"]("https://github.com/${release_repo}/releases/download/${tag}/${zip_name}")"
-        fi
-    fi
+Download: ["${tag}.img"]("https://github.com/${release_repo}/releases/download/${tag}/${tag}.img")"
 curl --data parse_mode=HTML --data chat_id=$TELEGRAM_CHAT --data sticker=CAADBQADGgEAAixuhBPbSa3YLUZ8DBYE --request POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendSticker
 
 else
--- config.sh
+++ config.sh
@@ -5,18 +5,11 @@ export GITHUB_EMAIL=""
 
 export device=""
 
-export ROM=""
 export ROM_DIR=""
-export ROM_VERSION=""
 export local_manifest_url=""
 export manifest_url=""
 export rom_vendor_name=""
 export branch=""
-export bacon="bacon"
-export buildtype=""
-export clean=""
-export generate_incremental=""
-export upload_recovery=""
 
 export ccache=""
 export ccache_size=""

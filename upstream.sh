5f3d9cdcf66a4b7b52d3c6bd0620921625bd86fa
JarlPenguin <jarlpenguin@outlook.com>
2019-08-11 15:28:35 +0300

    upstream: fix derp
    Signed-off-by: JarlPenguin <jarlpenguin@outlook.com>

--- upstream.sh
+++ upstream.sh
@@ -1,1 +1,2 @@
 function upstream() { for var in montana-los15 cactus-los16 montana-los16 montana-carbon7 montana-los14; do git checkout $var; git merge master -m "Automatic upstream merge" --signoff; git push; done; git checkout master; }
+upstream

6f8d01229f51ccedd1fd6b83427259d4741b37b7
JarlPenguin <jarlpenguin@outlook.com>
2019-08-11 18:21:48 +0300

    sync: remove local manifest before downloading
    Signed-off-by: JarlPenguin <jarlpenguin@outlook.com>

--- sync.sh
+++ sync.sh
@@ -3,6 +3,9 @@ echo "Sync started for "$manifest_url"/tree/"$branch""
 telegram -M "Sync started for ["$ROM" "$ROM_VERSION"]("$manifest_url"/tree/"$branch")"
 SYNC_START=$(date +"%s")
 mkdir -p .repo/local_manifests
+if [ -f .repo/local_manifests/manifest.xml ]; then
+rm .repo/local_manifests/manifest.xml
+fi
 wget "$local_manifest_url" -O .repo/local_manifests/manifest.xml
 repo sync --force-sync --current-branch --no-tags --no-clone-bundle --optimized-fetch --prune -j$(nproc --all) -q -c
 export synccompletedsuccessfully=$?

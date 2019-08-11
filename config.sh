bb1876660fa96e667cfec2376137c2228fd397be
Jarl-Penguin <jarlpenguin@outlook.com>
2019-08-12 00:54:14 +0300

    Cedric-ify
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- config.sh
+++ config.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="JarlPenguin"
+export GITHUB_EMAIL="jarlpenguin@outlook.com"
 
-export device=""
+export device="cedric"
 
-export ROM_DIR=""
+export ROM_DIR="${WORKSPACE}/twrp"
 export official="false"
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
+export local_manifest_url="https://github.com/JarlPenguin/manifests/raw/master/twrp/cedric.xml"
+export manifest_url="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni"
+export rom_vendor_name="omni"
+export branch="twrp-7.1"
 
-export ccache=""
-export ccache_size=""
+export ccache="true"
+export ccache_size="100"
 
-export jenkins="false"
+export jenkins="true"
 
-export release_repo=""
+export release_repo="JarlPenguin/releases"
 
 export timezone="UTC"

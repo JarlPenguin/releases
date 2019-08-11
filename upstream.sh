f75972d750a990d951737b551324d8bed445c908
JarlPenguin <jarlpenguin@outlook.com>
2019-08-11 15:35:45 +0300

    travis: updates
    Signed-off-by: JarlPenguin <jarlpenguin@outlook.com>

--- upstream.sh
+++
@@ -1,2 +0,0 @@
-function upstream() { for var in montana-los15 cactus-los16 montana-los16 montana-carbon7 montana-los14; do git checkout $var; git merge master -m "Automatic upstream merge" --signoff; git push; done; git checkout master; }
-upstream

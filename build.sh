6eb09484d379883e22dfb6eb106519ff8303bfc8
Jarl-Penguin <jarlpenguin@outlook.com>
2019-10-17 05:05:28 +0300

    build: nuke .md5 files as well
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- build.sh
+++ build.sh
@@ -15,6 +15,7 @@ if [ "${official}" == "true" ] || [ "${official}" == "1" ]; then
 fi
 lunch "${rom_vendor_name}_${device}-userdebug"
 rm "${outdir}"/*201*.zip
+rm "${outdir}"/*201*.zip.md5
 mka "${bacon}"
 BUILD_END=$(date +"%s")
 BUILD_DIFF=$((BUILD_END - BUILD_START))

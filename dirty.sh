3add830c967a7b5d374ce10c5fe58336236f211c
Jarl-Penguin <jarlpenguin@outlook.com>
2019-10-24 18:58:40 +0000

    releases: use -z instead of == ""
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- dirty.sh
+++ dirty.sh
@@ -5,7 +5,7 @@ export my_dir=$(pwd)
 echo "Loading configuration..."
 source "${my_dir}/config.sh"
 
-if [ "${GITHUB_TOKEN}" == "" ] || [ "${TELEGRAM_CHAT}" == "" ] || [ "${TELEGRAM_TOKEN}" == "" ]; then
+if [ -z "${GITHUB_TOKEN}" ] || [ -z "${TELEGRAM_CHAT}" ] || [ -z "${TELEGRAM_TOKEN}" ]; then
     echo "Please set GITHUB_TOKEN, TELEGRAM_CHAT, and TELEGRAM_TOKEN before continuing."
     exit 1
 fi

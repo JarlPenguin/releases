407e7cf1e5a14b1c60f8166dd457411e5cbec71a
Jarl-Penguin <jarlpenguin@outlook.com>
2020-08-24 09:57:44 +0300

    montana -> hannah
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- config.sh
+++ config.sh
@@ -3,13 +3,13 @@
 export GITHUB_USER="JarlPenguin"
 export GITHUB_EMAIL="jarlpenguin@gmail.com"
 
-export device="montana"
+export device="hannah"
 
 export ROM="LineageOS"
 export ROM_DIR="/var/lib/jenkins/los16"
 export ROM_VERSION="16"
 export official="false"
-export local_manifest_url="https://github.com/JarlPenguin/manifests/raw/master/lineage/16.0/montana.xml"
+export local_manifest_url="https://github.com/JarlPenguin/manifests/raw/master/lineage/16.0/hannah.xml"
 export manifest_url="https://github.com/LineageOS/android"
 export rom_vendor_name="lineage"
 export branch="lineage-16.0"

a8a0e79116ee6f5be0005cb900efa6bc7d514292
alexwcrafter <39865767+alexwcrafter@users.noreply.github.com>
2021-05-16 10:24:12 +0200

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -3,7 +3,7 @@
 export my_dir=$(pwd)
 
 echo "Loading configuration..."
-source "${my_dir}"/config.sh
+source "${my_dir}"/config.sh && source "${mydir}"/tgbot.sh
 
 if [ -z "${GITHUB_TOKEN}" ]; then
     echo "Please set GITHUB_TOKEN before continuing."

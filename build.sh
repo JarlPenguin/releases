16f67d06f57097c71639831f68ed822342f9f0a1
Jarl-Penguin <jarlpenguin@outlook.com>
2021-06-14 14:05:56 +0300

    Revert "[TEMP] Revert "Update build.sh""
    This reverts commit 267748498b95dd2ab5a27f5e4e50fc3b3dcc4a76.

--- build.sh
+++ build.sh
@@ -24,58 +24,20 @@ elif [ "${ccache}" == "true" ] && [ -z "${ccache_size}" ]; then
     exit 1
 fi
 lunch "${rom_vendor_name}_${device}-${buildtype}"
-rm "${outdir}"/*2021*.zip
-rm "${outdir}"/*2021*.zip.md5
 if [ "${clean}" == "clean" ]; then
     mka clean
     mka clobber
 elif [ "${clean}" == "installclean" ]; then
     mka installclean
 fi
-mka "${bacon}"
+mka dist
 BUILD_END=$(date +"%s")
 BUILD_DIFF=$((BUILD_END - BUILD_START))
-
-export finalzip_path=$(ls "${outdir}"/*2021*.zip | tail -n -1)
-if [ "${upload_recovery}" == "true" ]; then
-    export img_path=$(ls "${outdir}"/recovery.img | tail -n -1)
-fi
-export zip_name=$(echo "${finalzip_path}" | sed "s|${outdir}/||")
-export tag=$( echo "${zip_name}-$(date +%H%M)" | sed 's|.zip||')
-if [ -e "${finalzip_path}" ]; then
+export bootimage_path=$(ls "${outdir}"/boot.img | tail -n -1)
+if [ -e "${bootimage_path}" ]; then
     echo "Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds"
-
-    echo "Uploading"
-
-    github-release "${release_repo}" "${tag}" "master" "${ROM} for ${device}
-
-Date: $(env TZ="${timezone}" date)" "${finalzip_path}"
-    if [ "${upload_recovery}" == "true" ]; then
-        if [ -e "${img_path}" ]; then
-            github-release "${release_repo}" "${tag}" "master" "${ROM} for ${device}
-
-Date: $(env TZ="${timezone}" date)" "${img_path}"
-        else
-            echo "Build failed in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds"
-            telegram -N -M "Build failed in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds"
-            curl --data parse_mode=HTML --data chat_id=$TELEGRAM_CHAT --data sticker=CAADBQADGgEAAixuhBPbSa3YLUZ8DBYE --request POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendSticker
-            exit 1
-        fi
-    fi
-    echo "Uploaded"
-
-    if [ "${upload_recovery}" == "true" ]; then
-        telegram -M "Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds
-
-Download ROM: ["${zip_name}"]("https://github.com/${release_repo}/releases/download/${tag}/${zip_name}")
-Download recovery: ["recovery.img"]("https://github.com/${release_repo}/releases/download/${tag}/recovery.img")"
-    else
-        telegram -M "Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds
-
-Download: ["${zip_name}"]("https://github.com/${release_repo}/releases/download/${tag}/${zip_name}")"
-    fi
-curl --data parse_mode=HTML --data chat_id=$TELEGRAM_CHAT --data sticker=CAADBQADGgEAAixuhBPbSa3YLUZ8DBYE --request POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendSticker
-
+    telegram -M "Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds"
+    curl --data parse_mode=HTML --data chat_id=$TELEGRAM_CHAT --data sticker=CAADBQADGgEAAixuhBPbSa3YLUZ8DBYE --request POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendSticker
 else
     echo "Build failed in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds"
     telegram -N -M "Build failed in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds"

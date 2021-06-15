4c854b58d8a3c92b28fca93fa5c21d5e0c116cc5
Jarl-Penguin <36794857+JarlPenguin@users.noreply.github.com>
2021-06-15 08:51:55 +0300

    Installclean

--- config.sh
+++ config.sh
@@ -15,7 +15,7 @@ export rom_vendor_name="aosp"
 export branch="eleven"
 export bacon="dist"
 export buildtype=""
-export clean="clean"
+export clean="installclean"
 export upload_recovery="false"
 
 export ccache="true"

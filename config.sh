3a30b76043773456acef9f7b2d576edb763004df
Jarl-Penguin <jarlpenguin@outlook.com>
2021-07-03 18:34:41 +0300

    Montana-ify
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- config.sh
+++ config.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="JarlPenguin"
+export GITHUB_EMAIL="jarlpenguin@gmail.com"
 
-export device=""
+export device="montana"
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
+export ROM="ProtonAOSP"
+export ROM_DIR="${WORKSPACE}/../ProtonAOSP-12"
+export ROM_VERSION="12"
 export official="false"
-export local_manifest_url=""
-export manifest_url=""
+export local_manifest_url="https://github.com/JarlPenguin/manifests/raw/master/protonaosp/12/msm8937-common.xml"
+export manifest_url="https://github.com/ProtonAOSP/android_manifest"
 export rom_vendor_name=""
-export branch=""
-export bacon="bacon"
-export buildtype=""
-export clean=""
-export generate_incremental=""
-export upload_recovery=""
+export branch="sc-v2"
+export bacon="otapackage"
+export buildtype="user"
+export clean="installclean"
+export generate_incremental="false"
+export upload_recovery="true"
 
-export ccache=""
-export ccache_size=""
+export ccache="false"
+export ccache_size="100"
 
-export jenkins="false"
+export jenkins="true"
 
-export release_repo=""
+export release_repo="JarlPenguin/releases"
 
 export timezone="UTC"

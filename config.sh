a9a843901c3e88f632b1cf694c413015a13e6878
lobo1978-coder <wolf45734@gmail.com>
2021-10-03 15:53:51 +1100

    config.

--- config.sh
+++ config.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="lobo1978-coder"
+export GITHUB_EMAIL="wolf45734@gmail.com"
 
-export device=""
+export device="dumpling"
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
+export ROM="lineage"
+export ROM_DIR="/var/lib/jenkins/lineage"
+export ROM_VERSION="11"
 export official="false"
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
+export local_manifest_url="https://github.com/LineageOS/android/blob/lineage-18.1/default.xml"
+export manifest_url="repo init -u git://github.com/LineageOS/android.git -b lineage-18.1"
+export rom_vendor_name="lineage"
+export branch="18.1"
 export bacon="bacon"
-export buildtype=""
-export clean=""
-export generate_incremental=""
-export upload_recovery=""
+export buildtype="userdebug"
+export clean="installclean"
+export generate_incremental="true"
+export upload_recovery="false"
 
-export ccache=""
-export ccache_size=""
+export ccache="true"
+export ccache_size="50"
 
-export jenkins="false"
+export jenkins="true"
 
-export release_repo=""
+export release_repo="https://github.com/lobo1978-coder/releases"
 
 export timezone="UTC"

09a1b9f64c69546b904ddf05c647fe16685fa022
Jarl-Penguin <jarlpenguin@outlook.com>
2021-11-23 16:38:43 +0300

    build: Account for empty rom_vendor_name
    * See ProtonAOSP
    
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- build.sh
+++ build.sh
@@ -23,7 +23,11 @@ elif [ "${ccache}" == "true" ] && [ -z "${ccache_size}" ]; then
     echo "Please set the ccache_size variable in your config."
     exit 1
 fi
-lunch "${rom_vendor_name}_${device}-${buildtype}"
+if [ ! -z "${rom_vendor_name}" ]; then
+    lunch "${rom_vendor_name}_${device}-${buildtype}"
+else
+    lunch "${device}-${buildtype}"
+fi
 if [ "${clean}" == "clean" ]; then
     m clean -j$(nproc --all)
 elif [ "${clean}" == "installclean" ]; then

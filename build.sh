36b37b144ad9c7f39d981ec93f7f389b2527dacb
kiam001 <blablatupfen@gmail.com>
2022-07-08 17:13:49 +0200

    build: Fix disabling ccache

--- build.sh
+++ build.sh
@@ -13,6 +13,12 @@ source "${my_dir}/config.sh"
 if [ -z "${buildtype}" ]; then
     export buildtype="userdebug"
 fi
+if [ "${ccache}" == "false" ]; then
+  echo "DEBUG CCACHE DISABLE BY SCRIPT"
+  unset USE_CCACHE
+  unset CCACHE_DIR
+  unset CCACHE_EXEC
+fi
 if [ "${ccache}" == "true" ] && [ -n "${ccache_size}" ]; then
     export USE_CCACHE=1
     ccache -M "${ccache_size}G"

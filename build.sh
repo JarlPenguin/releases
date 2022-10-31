2ad7d9f06ca4fc0b18351f6f3ecb23ee66c91908
Jarl-Penguin <jarlpenguin@outlook.com>
2022-10-31 22:47:30 +0300

    Camera
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- build.sh
+++ build.sh
@@ -42,92 +42,11 @@ buildsuccessful="${?}"
 BUILD_END=$(date +"%s")
 BUILD_DIFF=$((BUILD_END - BUILD_START))
 
-if [ "${generate_incremental}" == "true" ]; then
-    if [ -e "${ROM_DIR}"/*"${device}"*target_files*.zip ]; then
-        export old_target_files_exists=true
-        export old_target_files_path=$(ls "${ROM_DIR}"/*"${device}"*target_files*.zip | tail -n -1)
-    else
-        echo "Old target-files package not found, generating incremental package on next build"
-    fi
-    export new_target_files_path=$(ls "${outdir}"/obj/PACKAGING/target_files_intermediates/*target_files*.zip | tail -n -1)
-    if [ "${old_target_files_exists}" == "true" ]; then
-        ota_from_target_files -i "${old_target_files_path}" "${new_target_files_path}" "${outdir}"/incremental_ota_update.zip
-        export incremental_zip_path=$(ls "${outdir}"/incremental_ota_update.zip | tail -n -1)
-    fi
-    cp "${new_target_files_path}" "${ROM_DIR}"
-fi
-if [ -e "${outdir}"/*$(date +%Y)*.zip ]; then
-    export finalzip_path=$(ls "${outdir}"/*$(date +%Y)*.zip | tail -n -1)
-else
-    export finalzip_path=$(ls "${outdir}"/*"${device}"-ota-*.zip | tail -n -1)
-fi
-if [ "${upload_recovery}" == "true" ]; then
-    if [ ! -e "${outdir}"/recovery.img ]; then
-        cp "${outdir}"/boot.img "${outdir}"/recovery.img
-    fi
-    export img_path=$(ls "${outdir}"/recovery.img | tail -n -1)
-fi
-export zip_name=$(echo "${finalzip_path}" | sed "s|${outdir}/||")
-export tag=$( echo "$(env TZ="${timezone}" date +%Y%m%d%H%M)-${zip_name}" | sed 's|.zip||')
 if [ "${buildsuccessful}" == "0" ] && [ ! -z "${finalzip_path}" ]; then
     echo "Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds"
 
-    echo "Uploading"
+    telegram -M "Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds"
 
-    github-release "${release_repo}" "${tag}" "master" "${ROM} for ${device}
-
-Date: $(env TZ="${timezone}" date)" "${finalzip_path}"
-    if [ "${generate_incremental}" == "true" ]; then
-        if [ -e "${incremental_zip_path}" ] && [ "${old_target_files_exists}" == "true" ]; then
-            github-release "${release_repo}" "${tag}" "master" "${ROM} for ${device}
-
-Date: $(env TZ="${timezone}" date)" "${incremental_zip_path}"
-        elif [ ! -e "${incremental_zip_path}" ] && [ "${old_target_files_exists}" == "true" ]; then
-            echo "Build failed in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds"
-            telegram -N -M "Build failed in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds"
-            curl --data parse_mode=HTML --data chat_id=$TELEGRAM_CHAT --data sticker=CAADBQADGgEAAixuhBPbSa3YLUZ8DBYE --request POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendSticker
-            exit 1
-        fi
-    fi
-    if [ "${upload_recovery}" == "true" ]; then
-        if [ -e "${img_path}" ]; then
-            github-release "${release_repo}" "${tag}" "master" "${ROM} for ${device}
-
-Date: $(env TZ="${timezone}" date)" "${img_path}"
-        else
-            echo "Build failed in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds"
-            telegram -N -M "Build failed in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds"
-            curl --data parse_mode=HTML --data chat_id=$TELEGRAM_CHAT --data sticker=CAADBQADGgEAAixuhBPbSa3YLUZ8DBYE --request POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendSticker
-            exit 1
-        fi
-    fi
-    echo "Uploaded"
-
-    if [ "${upload_recovery}" == "true" ]; then
-        if [ "${old_target_files_exists}" == "true" ]; then
-            telegram -M "Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds
-
-Download ROM: ["${zip_name}"]("https://github.com/${release_repo}/releases/download/${tag}/${zip_name}")
-Download incremental update: ["incremental_ota_update.zip"]("https://github.com/${release_repo}/releases/download/${tag}/incremental_ota_update.zip")
-Download recovery: ["recovery.img"]("https://github.com/${release_repo}/releases/download/${tag}/recovery.img")"
-        else
-            telegram -M "Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds
-
-Download ROM: ["${zip_name}"]("https://github.com/${release_repo}/releases/download/${tag}/${zip_name}")
-Download recovery: ["recovery.img"]("https://github.com/${release_repo}/releases/download/${tag}/recovery.img")"
-        fi
-    else
-        if [ "${old_target_files_exists}" == "true" ]; then
-            telegram -M "Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds
-
-Download: ["${zip_name}"]("https://github.com/${release_repo}/releases/download/${tag}/${zip_name}")
-Download incremental update: ["incremental_ota_update.zip"]("https://github.com/${release_repo}/releases/download/${tag}/incremental_ota_update.zip")"
-        else
-            telegram -M "Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds
-
-Download: ["${zip_name}"]("https://github.com/${release_repo}/releases/download/${tag}/${zip_name}")"
-        fi
-    fi
 curl --data parse_mode=HTML --data chat_id=$TELEGRAM_CHAT --data sticker=CAADBQADGgEAAixuhBPbSa3YLUZ8DBYE --request POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendSticker
 
 else

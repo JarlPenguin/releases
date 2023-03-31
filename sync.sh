2db75bb1e81d16a386b4a6cde30ee5507bc05d19
Jarl-Penguin <jarlpenguin@outlook.com>
2023-03-31 21:06:46 +0300

    sync: Sync with default amount of cores
    * Otherwise headache ensues...
    
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- sync.sh
+++ sync.sh
@@ -10,11 +10,7 @@ rm -rf .repo/local_manifests
 mkdir -p .repo/local_manifests
 wget "${local_manifest_url}" -O .repo/local_manifests/manifest.xml
 repo init -u "${manifest_url}" -b "${branch}" --depth 1
-cores=$(nproc --all)
-if [ "${cores}" -gt "8" ]; then
-    cores=8
-fi
-repo sync --force-sync --fail-fast --no-tags --no-clone-bundle --optimized-fetch --prune "-j${cores}" -c -v
+repo sync --force-sync --fail-fast --no-tags --no-clone-bundle --optimized-fetch --prune -c -v
 syncsuccessful="${?}"
 SYNC_END=$(date +"%s")
 SYNC_DIFF=$((SYNC_END - SYNC_START))

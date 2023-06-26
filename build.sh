323c47c7d6dbeedd8f297aadf2653de4afe66de1
Jarl-Penguin <jarlpenguin@outlook.com>
2023-06-26 21:49:23 +0300

    build: Link to releases instead of direct file
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- build.sh
+++ build.sh
@@ -110,25 +110,25 @@ Date: $(env TZ="${timezone}" date)" "${img_path}"
         if [ "${old_target_files_exists}" == "true" ]; then
             telegram -M "Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds
 
-Download ROM: ["${zip_name}"]("https://github.com/${release_repo}/releases/download/${tag}/${zip_name}")
+Download ROM: ["${zip_name}"]("https://github.com/${release_repo}/releases/tag/${tag}")
 Download incremental update: ["incremental_ota_update.zip"]("https://github.com/${release_repo}/releases/download/${tag}/incremental_ota_update.zip")
 Download recovery: ["recovery.img"]("https://github.com/${release_repo}/releases/download/${tag}/recovery.img")"
         else
             telegram -M "Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds
 
-Download ROM: ["${zip_name}"]("https://github.com/${release_repo}/releases/download/${tag}/${zip_name}")
+Download ROM: ["${zip_name}"]("https://github.com/${release_repo}/releases/tag/${tag}")
 Download recovery: ["recovery.img"]("https://github.com/${release_repo}/releases/download/${tag}/recovery.img")"
         fi
     else
         if [ "${old_target_files_exists}" == "true" ]; then
             telegram -M "Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds
 
-Download: ["${zip_name}"]("https://github.com/${release_repo}/releases/download/${tag}/${zip_name}")
+Download: ["${zip_name}"]("https://github.com/${release_repo}/releases/tag/${tag}")
 Download incremental update: ["incremental_ota_update.zip"]("https://github.com/${release_repo}/releases/download/${tag}/incremental_ota_update.zip")"
         else
             telegram -M "Build completed successfully in $((BUILD_DIFF / 60)) minute(s) and $((BUILD_DIFF % 60)) seconds
 
-Download: ["${zip_name}"]("https://github.com/${release_repo}/releases/download/${tag}/${zip_name}")"
+Download: ["${zip_name}"]("https://github.com/${release_repo}/releases/tag/${tag}")
         fi
     fi
 curl --data parse_mode=HTML --data chat_id=$TELEGRAM_CHAT --data sticker=CAADBQADGgEAAixuhBPbSa3YLUZ8DBYE --request POST https://api.telegram.org/bot$TELEGRAM_TOKEN/sendSticker
